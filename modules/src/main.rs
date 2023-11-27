1d153eb733f0608e40d91fe2ec80b397ac219102
Simone Cianni <simone.cianni@adesso.ch>
2023-11-27 16:32:56 +0100

    use
    Please note that use has nothing to do with importing,
    and simply specifies visibility of module names
    
    Signed-off-by: Simone Cianni <simone.cianni@adesso.ch>

--- modules/src/main.rs
+++ modules/src/main.rs
@@ -6,4 +6,15 @@ fn main() {
     let res = boo::answer();
     let q = boo::bar::question();
     println!("{:?} {} {}", f, res, q);
+
+    {
+        use boo::bar;
+        let q = bar::question();
+        println!("{}", q);
+    }
+    {
+        use boo::bar::question;
+        let q = question();
+        println!("{}", q);
+    }
 }
